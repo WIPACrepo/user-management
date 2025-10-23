9a2260323bf87a264556c95b3380f5d4bbd686e4
David Schultz <david.schultz@icecube.wisc.edu>
2025-10-23 11:53:14 -0500

    update version

--- user_mgmt/__init__.py
+++ user_mgmt/__init__.py
@@ -1,5 +1,3 @@
-__version__ = '1.3.21'
-# TODO: ^^^ remove __version__ (keep comment below)
 # NOTE: `__version__` is not defined because this package is built using 'setuptools-scm' --
 #   use `importlib.metadata.version(...)` if you need to access version info at runtime.
 
