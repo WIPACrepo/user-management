2c5ecb8ddcca968c23a16fa2d05342e24de2942f
David Schultz <david.schultz@icecube.wisc.edu>
2025-10-23 11:53:44 -0500

    fix blank line

--- user_mgmt/__init__.py
+++ user_mgmt/__init__.py
@@ -1,3 +1,2 @@
 # NOTE: `__version__` is not defined because this package is built using 'setuptools-scm' --
 #   use `importlib.metadata.version(...)` if you need to access version info at runtime.
-
